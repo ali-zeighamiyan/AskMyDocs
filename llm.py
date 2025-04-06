42015185de1bfe0ca9f484a1bbdb9f22987d1934
ali-zeighamiyan <azb1378@gmail.com>
2025-04-06 19:55:41 +0330

    Refactor LLM class to integrate with Telegram bot session flow

--- llm.py
+++ llm.py
@@ -17,9 +17,6 @@ from langchain.docstore.document import Document
 from langchain_core.messages import HumanMessage, AIMessage
 import streamlit as st
 
-
-
-
 load_dotenv()
 groq_api_key = os.getenv("Groq_API_KE")
 cohere_api_key = os.getenv("Cohere_API_KEY")
@@ -27,7 +24,7 @@ cohere_api_key = os.getenv("Cohere_API_KEY")
 directory_path = os.path.dirname(os.path.abspath("__file__"))
 
 class LLM:
-    def __init__(self):        
+    def __init__(self, user_id="default"):        
         self.llm = ChatGroq(api_key=groq_api_key, temperature=0, model_name="llama-3.3-70b-specdec")
         self.embedding_model = CohereEmbeddings(cohere_api_key=cohere_api_key, model="embed-english-light-v3.0")
         if "his_messages" not in st.session_state:
@@ -42,8 +39,9 @@ class LLM:
                                                         MessagesPlaceholder(variable_name="chat_history"),
                                                         ("human", human)])
         self.filter = None
+        self.user_id = user_id
         if not os.path.exists("log.json"):
-            self.log = {"EmbeddedDocsName":{}}
+            self.log = {self.user_id: {"EmbeddedDocsName":{}}}
             self.save_log()
         else:
             self.load_log()
@@ -61,7 +59,8 @@ class LLM:
         return ai_message.content
     
     def load_docs(self, selected_docs_name, cluster_name):
-        file_paths = [os.path.join("temp", cluster_name, f_name) for f_name in selected_docs_name if f_name not in self.log["EmbeddedDocsName"].get(cluster_name, [])]
+        file_paths = [os.path.join("temp", self.user_id, cluster_name, f_name) for f_name in selected_docs_name if f_name not
+                      in self.log[self.user_id]["EmbeddedDocsName"].get(cluster_name, [])]
         self.documents = []
         for file_path in file_paths:
             if file_path.endswith(".pdf"):
@@ -74,10 +73,10 @@ class LLM:
             docs_with_metadata = [Document(page_content=doc.page_content,  metadata={"source": file_name}) 
                                   for doc in docs]
             self.documents.extend(docs_with_metadata)
-            if cluster_name not in self.log["EmbeddedDocsName"]:
-                self.log["EmbeddedDocsName"][cluster_name] = [file_name]
+            if cluster_name not in self.log[self.user_id]["EmbeddedDocsName"]:
+                self.log[self.user_id]["EmbeddedDocsName"][cluster_name] = [file_name]
             else:
-                self.log["EmbeddedDocsName"][cluster_name].append(file_name)
+                self.log[self.user_id]["EmbeddedDocsName"][cluster_name].append(file_name)
                 
         
     def update_rag(self):
@@ -138,9 +137,9 @@ class LLM:
         return self.final_faiss_db
     
     def save_db(self, cluster_name):
-        self.final_faiss_db.save_local(os.path.join(directory_path, "faiss_dbs", cluster_name))
+        self.final_faiss_db.save_local(os.path.join(directory_path, "faiss_dbs", self.user_id, cluster_name))
     def load_db(self, cluster_name):
-        db_path = os.path.join(directory_path, "faiss_dbs", cluster_name)
+        db_path = os.path.join(directory_path, "faiss_dbs", self.user_id, cluster_name)
         loaded_db = FAISS.load_local(db_path, self.embedding_model, allow_dangerous_deserialization=True)
         if self.final_faiss_db:
             self.final_faiss_db.merge_from(loaded_db)
@@ -155,6 +154,25 @@ class LLM:
         text_chunks = self.split_documents(self.documents)
         splitted_chunks = self.split_chunks_to_max_limited(text_chunks)
         self.build_db(splitted_chunks)
+        
+    def load_or_save_doc(self, cluster_name, selected_docs_name):
+        new_docs_name = set(selected_docs_name) - set(self.log[self.user_id]["EmbeddedDocsName"].get(cluster_name, []))
+        common_docs_name = set(selected_docs_name).intersection(self.log[self.user_id]["EmbeddedDocsName"].get(cluster_name, []))
+        if new_docs_name : 
+            print("new docs ... processing ....")
+            self.process_new_docs(new_docs_name, cluster_name)
+        if common_docs_name:
+            print("old docs .... loading ...")
+            self.load_db(cluster_name)
+        self.save_db(cluster_name)
+        print("DB has saved ... ")
+        self.save_log()
+    
+    def run_chain(self, selected_docs_name, question):
+        self.get_retriever(selected_docs_name)
+        self.update_rag()
+        ai_ans = self.get_model_prediction_with_rag(question=question)
+        return ai_ans
         
     def run(self, question, cluster_name, selected_docs_name):
         new_docs_name = set(selected_docs_name) - set(self.log["EmbeddedDocsName"].get(cluster_name, []))
